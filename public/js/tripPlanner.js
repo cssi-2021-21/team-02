27cf15627fa7ee39f3f30ccf5ee38602ae3c8c58
Divya <singhaldivya11@gmail.com>
2021-08-03 18:41:24 +0000

    pulling changes

--- public/js/tripPlanner.js
+++ public/js/tripPlanner.js
@@ -44,7 +44,41 @@ function createTrip () {
       .then(() => {
         console.log("removing hidden");
         tripDetailSection.classList.remove("hidden");
+
+        let myKey = 'AIzaSyDhcyOsHp-sFAtQhsTahxpCRGfCHfxphYY';
+        let destPic = destinationChoice.value;
+
+        let myQuery = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + destPic + ', IL&key=' + myKey + '&inputtype=textquery&fields=name,photos';
+
       }); 
 
 }
+
+submitButton.addEventListener("click", (e) => {
+  let myKey = 'amaZYdG5z8O5726r5QxdDAv2MYEw3vpY';
+  let topicChoice = queryField.value;
+  console.log(topicChoice);
+  topicText.innerHTML = "Topic: " + topicChoice;
+  document.getElementById("answerSubmit").disabled = false;
+  for (let i = 0; i < topicArray.length; i++){
+    for (let j = 0; j < topicArray[i].length; j++){
+      console.log(topicArray[i][j]);
+      var questionText = "";
+      if (topicArray[i][j] == topicChoice){
+        let topicID = topicArray[i][j+1];
+        console.log(topicID);
+        let myQuery = `https://jservice.io/api/clues?category=` + topicID; 
+        fetch(myQuery)
+          .then(response => response.json())
+          .then(myjson => {
+          console.log(myjson[0].question); 
+          const questionText = myjson[0].question;
+          actualQuestion.innerHTML = questionText;
+        });
+        
+    } 
+  }
+}
+});
+
 
