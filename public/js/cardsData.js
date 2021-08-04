fb0b34fe0a5afffbbdf45702afb3ab0b6384e3a0
David Chen <davidc3287@gmail.com>
2021-08-04 13:36:46 -0400

    load images to homepage

--- public/js/cardsData.js
+++ public/js/cardsData.js
@@ -3,7 +3,7 @@ let cardsData = [];
 firebase
   .database()
   .ref("users")
-  .on("value", (snapshot) => {
+  .on("value", async (snapshot) => {
     cardsData.length = 0;
     const data = snapshot.val();
     for (let key in data) {
@@ -22,9 +22,10 @@ firebase
       for (let key2 in userData) {
         let map = userData[key2];
         if (!("firstName" in map)) {
+          let img = await loadImages(map["destination"]);
           let card = {
             id: 1,
-            image: loadImages(map["destination"]),
+            image: img,
             avatar:
               "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
             user: {
@@ -39,7 +40,7 @@ firebase
     }
   });
 
-const loadImages = (destination) => {
+const loadImages = async (destination) => {
   let myKey = "AIzaSyDhcyOsHp-sFAtQhsTahxpCRGfCHfxphYY";
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
@@ -49,28 +50,27 @@ const loadImages = (destination) => {
     "&key=" +
     myKey +
     "&inputtype=textquery&fields=name,photos";
-  fetch(proxyurl + myQuery, {
-    //mode: 'no-cors'
-  })
-    .then((response) => response.json())
-    .then((myjson) => {
-      let photoRef = myjson.candidates[0].photos[0];
-      let photoreference = myjson.candidates[0].photos[0].photo_reference;
-      let myQuery2 =
-        "https://maps.googleapis.com/maps/api/place/photo?photoreference=" +
-        photoreference +
-        "&key=" +
-        myKey +
-        "&maxwidth=400&maxheight=400";
 
-      fetch(proxyurl + myQuery2)
-        .then((response) => response.blob())
-        .then((images) => {
-          var img = URL.createObjectURL(images);
-          console.log("Image: ", img);
-          return img;
-        });
-    });
+  let resp = await fetch(proxyurl + myQuery);
+  console.log(resp);
+  let myjson = await resp.json();
+  console.log(myjson);
+
+  // let photoRef = myjson.candidates[0].photos[0];
+  let photoreference = myjson.candidates[0].photos[0].photo_reference;
+  let myQuery2 =
+    "https://maps.googleapis.com/maps/api/place/photo?photoreference=" +
+    photoreference +
+    "&key=" +
+    myKey +
+    "&maxwidth=400&maxheight=400";
+
+  let images = await (await fetch(proxyurl + myQuery2)).blob();
+
+  var img = URL.createObjectURL(images);
+  console.log("Image: ", img);
+
+  return img;
 };
 
 var app = new Vue({
