9d70aaec88a310a0f59f4af7b3522f9cb32b18f3
David Chen <davidc3287@gmail.com>
2021-08-04 14:45:16 -0400

    manual firebaes data

--- public/js/cardsData.js
+++ public/js/cardsData.js
@@ -1,44 +1,59 @@
 let cardsData = [];
 
-firebase
-  .database()
-  .ref("users")
-  .on("value", async (snapshot) => {
-    cardsData.length = 0;
-    const data = snapshot.val();
-    for (let key in data) {
-      const userData = data[key];
-      let name = "Guest 1531";
-      for (let key2 in userData) {
-        // grab user name
-        let map = userData[key2];
-        if ("firstName" in map) {
-          // user data
-          name = map["firstName"] + " " + map["lastName"];
-          break;
-        }
-      }
+// let card = {
+//   id: 1,
+//   image: "https://source.unsplash.com/h-ACUrBngrw/1280x720",
+//   avatar:
+//     "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+//   user: {
+//     handle: name,
+//     title: "Tokyo",
+//   },
+//   content: "From: sometime to sometime",
+// };
+// cardsData.push(card);
 
-      for (let key2 in userData) {
-        let map = userData[key2];
-        if (!("firstName" in map)) {
-          let img = await loadImages(map["destination"]);
-          let card = {
-            id: 1,
-            image: img,
-            avatar:
-              "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
-            user: {
-              handle: name,
-              title: map["destination"],
-            },
-            content: "From: " + map["firstDate"] + " to " + map["lastDate"],
-          };
-          cardsData.push(card);
+const loadFirebase = () => {
+  firebase
+    .database()
+    .ref("users")
+    .on("value", async (snapshot) => {
+      cardsData.length = 0;
+      const data = snapshot.val();
+      for (let key in data) {
+        const userData = data[key];
+        let name = "Guest 1531";
+        for (let key2 in userData) {
+          // grab user name
+          let map = userData[key2];
+          if ("firstName" in map) {
+            // user data
+            name = map["firstName"] + " " + map["lastName"];
+            break;
+          }
+        }
+
+        for (let key2 in userData) {
+          let map = userData[key2];
+          if (!("firstName" in map)) {
+            let img = await loadImages(map["destination"]);
+            let card = {
+              id: 1,
+              image: img,
+              avatar:
+                "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+              user: {
+                handle: name,
+                title: map["destination"],
+              },
+              content: "From: " + map["firstDate"] + " to " + map["lastDate"],
+            };
+            cardsData.push(card);
+          }
         }
       }
-    }
-  });
+    });
+};
 
 const loadImages = async (destination) => {
   let myKey = "AIzaSyDhcyOsHp-sFAtQhsTahxpCRGfCHfxphYY";
@@ -72,6 +87,8 @@ const loadImages = async (destination) => {
 
   return img;
 };
+
+loadFirebase();
 
 var app = new Vue({
   el: "#app",
