b2ff66cf83f2a8999fb69a40e445b668271eb517
nithilap <nithila@poongo.net>
2021-08-05 17:47:44 +0000

    Adding Base 64 Conversion for Images

--- public/js/pictureGallery.js
+++ public/js/pictureGallery.js
@@ -37,3 +37,18 @@ fileButton.addEventListener('change', function(e) {
     console.log("File Uploaded!");
     }
 });
+
+//Base 64
+function getBase64(file) {
+   var reader = new FileReader();
+   reader.readAsDataURL(file);
+   reader.onload = function () {
+     console.log(reader.result);
+   };
+   reader.onerror = function (error) {
+     console.log('Error: ', error);
+   };
+}
+
+var file = document.querySelector('#files > input[type="file"]').files[0];
+getBase64(file); // prints the base64 string
