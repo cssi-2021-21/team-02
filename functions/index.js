48f617678239da000f3bab168da815e3b7ead579
David Chen <davidc3287@gmail.com>
2021-08-05 13:20:48 -0400

    add profanity function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
+const Filter = require("bad-words");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.detectEvilUsers = functions.database
+  .ref("/messages/{msgId}")
+  .onCreate(async (snapshot, ctx) => {
+    const filter = new Filter();
+    functions.logger.log("*", snapshot.val());
+    const { msg, usr } = snapshot.val();
+
+    if (filter.isProfane(text)) {
+      const cleaned = filter.clean(msg);
+      await doc.ref.update({
+        msg: `${cleaned}`,
+      });
+    }
+  });
