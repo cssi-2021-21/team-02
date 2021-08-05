69c493527af34342a418bdf4064205fe4e3611e8
nithilap <nithila@poongo.net>
2021-08-05 16:22:27 +0000

    Updated picture gallery to choose personal files

--- public/js/pictureGallery.js
+++ public/js/pictureGallery.js
@@ -1,3 +1,4 @@
+console.log("code running.")
 const heartClicked = (id) => {
   // toggle heart
   let heartButton = document.querySelector(`#${id}`);
@@ -10,3 +11,29 @@ const heartClicked = (id) => {
   // TODO: database stuff
   console.log("heart toggled");
 };
+
+//Get elements
+
+var fileButton = document.getElementById('file-upload');
+
+//Listen for File Selection
+
+fileButton.addEventListener('change', function(e) {
+    //Get file
+    var file = e.target.files[0];
+    console.log(file);
+    var read = new FileReader();
+
+    read.readAsBinaryString(file);
+    console.log("Contents of file found");
+    read.onloadend = function(){
+      console.log(read.result);
+      //Create a storage reference
+    var storageRef = firebase.storage().ref('user_uploads/' + file.name);
+
+    //Upload file
+    storageRef.put(file);
+
+    console.log("File Uploaded!");
+    }
+});
