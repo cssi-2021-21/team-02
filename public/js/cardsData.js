74a9644797a75774790a5c66ebca67bb2c8ca07b
David Chen <davidc3287@gmail.com>
2021-08-05 14:19:45 -0400

    harcode cards

--- public/js/cardsData.js
+++ public/js/cardsData.js
@@ -1,17 +1,86 @@
-let cardsData = [];
-
-let card = {
-  id: 1,
-  image: "https://source.unsplash.com/h-ACUrBngrw/1280x720",
-  avatar:
-    "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
-  user: {
-    handle: name,
-    title: "Tokyo",
+let cardsData = [
+  {
+    id: 1,
+    image: "https://media.timeout.com/images/105122240/1372/1029/image.jpg",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "Divya",
+      title: "Melbourne",
+    },
+    content: "From: 8/5 to 8/11",
   },
-  content: "From: sometime to sometime",
-};
-cardsData.push(card);
+  {
+    id: 1,
+    image: "https://media.timeout.com/images/105403923/1372/1029/image.jpg",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "David",
+      title: "Berlin",
+    },
+    content: "From: 9/11 to 9/27",
+  },
+  {
+    id: 1,
+    image:
+      "https://media.timeout.com/images/105404089/1372/1029/image.jpghttps://source.unsplash.com/h-ACUrBngrw/1280x720",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "David",
+      title: "Tokyo",
+    },
+    content: "From: 10/27 to 10/38",
+  },
+  {
+    id: 1,
+    image: "https://media.timeout.com/images/105403930/1372/1029/image.jpg",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "Nithila",
+      title: "Paris",
+    },
+    content: "From: 12/12 to 12/14",
+  },
+  {
+    id: 1,
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/28/2021/02/19/new-york-city-evening-NYCTG0221.jpg",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "David",
+      title: "New York City",
+    },
+    content: "From: 8/11 to 8/12",
+  },
+  {
+    id: 1,
+    image: "https://media.timeout.com/images/105403950/1372/1029/image.jpg",
+    avatar:
+      "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+    user: {
+      handle: "Nithila",
+      title: "Mumbai",
+    },
+    content: "From: 9/5 to 9/15",
+  },
+];
+
+// let card = {
+//   id: 1,
+//   image: "https://source.unsplash.com/h-ACUrBngrw/1280x720",
+//   avatar:
+//     "https://banner2.cleanpng.com/20190221/gw/kisspng-computer-icons-user-profile-clip-art-portable-netw-c-svg-png-icon-free-download-389-86-onlineweb-5c6f7efd8fecb7.6156919015508108775895.jpg",
+//   user: {
+//     handle: name,
+//     title: "Tokyo",
+//   },
+//   content: "From: 8/5 to 8/11",
+// };
+// cardsData.push(card);
 
 const loadFirebase = () => {
   firebase
